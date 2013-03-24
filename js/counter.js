d6c8151520d7586a197a5c53d2e9d3b5e11056af
Baris Sencan <run32a50mch@gmail.com>
2013-03-24 15:11:05 +0200

    Down count now include remaining days count. Also, bit smaller dice dude.

--- js/counter.js
+++ js/counter.js
@@ -6,12 +6,15 @@ function setupCounter(elementId)
   {
     var time_left = Math.round(kontakt.getTime() - new Date().getTime()) / 1000;
 
+    var days_left = Math.floor(time_left / 86400);
+    time_left -= days_left * 86400;
+
     var hours_left = Math.floor(time_left / 3600);
     time_left -= hours_left * 3600;
 
     var minutes_left = Math.floor(time_left / 60);
     time_left -= minutes_left * 60;
 
-    document.getElementById(elementId).innerHTML = hours_left + ' saat<br />' + minutes_left + ' dakika<br />' + Math.floor(time_left) + ' saniye kaldı';
+    document.getElementById(elementId).innerHTML = days_left + ' gün<br />' + hours_left + ' saat<br />' + minutes_left + ' dakika<br />' + Math.floor(time_left) + ' saniye kaldı';
   }, 1000);
 }
