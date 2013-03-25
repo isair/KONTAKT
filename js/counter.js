c56c3ddd892e5811dcce13c2345dc9e94b206fe8
Baris Sencan <run32a50mch@gmail.com>
2013-03-25 22:06:13 +0200

    Counter now handles cases where current date is past event date. Config.xml added for Phongegap Build. iOS icons.

--- js/counter.js
+++ js/counter.js
@@ -1,20 +1,34 @@
 var kontakt = new Date(2013, 3, 6, 11, 0, 0);
+var kontakt_end = new Date(2013, 3, 7, 20, 0, 0);
 
 function setupCounter(elementId)
 {
   setInterval(function()
   {
-    var time_left = Math.round(kontakt.getTime() - new Date().getTime()) / 1000;
+    var now = new Date();
 
-    var days_left = Math.floor(time_left / 86400);
-    time_left -= days_left * 86400;
+    if (kontakt > now)
+    {
+      var time_left = Math.round(kontakt.getTime() - now.getTime()) / 1000;
 
-    var hours_left = Math.floor(time_left / 3600);
-    time_left -= hours_left * 3600;
+      var days_left = Math.floor(time_left / 86400);
+      time_left -= days_left * 86400;
 
-    var minutes_left = Math.floor(time_left / 60);
-    time_left -= minutes_left * 60;
+      var hours_left = Math.floor(time_left / 3600);
+      time_left -= hours_left * 3600;
 
-    document.getElementById(elementId).innerHTML = days_left + ' gün<br />' + hours_left + ' saat<br />' + minutes_left + ' dakika<br />' + Math.floor(time_left) + ' saniye kaldı';
+      var minutes_left = Math.floor(time_left / 60);
+      time_left -= minutes_left * 60;
+
+      document.getElementById(elementId).innerHTML = days_left + ' gün<br />' + hours_left + ' saat<br />' + minutes_left + ' dakika<br />' + Math.floor(time_left) + ' saniye kaldı';
+    }
+    else if (kontakt_end > now)
+    {
+      document.getElementById(elementId).innerHTML = 'KONTAKT 3<br />başladı!';
+    }
+    else
+    {
+      document.getElementById(elementId).innerHTML = 'KONTAKT 3<br />sona erdi.';
+    }
   }, 1000);
 }
